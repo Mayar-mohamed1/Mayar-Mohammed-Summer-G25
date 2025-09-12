0fb27ce1cd38b9b52b9c7c6a493fdc849f991acc
Mayar-mohamed1 <mayarabdelbaset2016@gmail.com>
2025-09-12 04:31:27 +0300

    Signed-off-by: Mayar-mohamed1 <mayarabdelbaset2016@gmail.com>

--- Merto Final Project/JS/script.js
+++ Merto Final Project/JS/script.js
@@ -1,2 +1,23 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+$(document).ready(function(){
+  $(".owl-carousel2").owlCarousel({
+     items:5,
+     margin:1,
+     loop:true,
+  
+ 
+     autoplay:true,
+     
+
+  });
+});
+
+
+/*    
+navText:['<i class="fa-solid fa-angle-left"></i>','<i class="fa-solid fa-angle-right"></i>'],
+   nav:true,
+*/
+
