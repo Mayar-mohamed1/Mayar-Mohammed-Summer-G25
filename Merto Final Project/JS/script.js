c67685233771d1420518314cec99bf8fc68e5db2
Mayar-mohamed1 <mayarabdelbaset2016@gmail.com>
2025-09-12 08:47:27 +0300

    contact new

--- Merto Final Project/JS/script.js
+++ Merto Final Project/JS/script.js
@@ -15,9 +15,35 @@ $(document).ready(function(){
   });
 });
 
+$(document).ready(function(){
+  $(".owl-carousel1").owlCarousel({
+     items:5,
+     margin:1,
+     loop:true,
+     nav:true,
+     navText:['<i class="fa-solid fa-angle-left"></i>','<i class="fa-solid fa-angle-right"></i>']
+  
+ 
+   
+     
 
-/*    
-navText:['<i class="fa-solid fa-angle-left"></i>','<i class="fa-solid fa-angle-right"></i>'],
-   nav:true,
-*/
+  });
+});
+
+
+
+window.addEventListener("scroll", ()=>{
+   var upArrow=document.getElementById("upArrow")
+   if(document.documentElement.scrollTop>300)
+      upArrow.classList.remove("d-none")
+
+ 
+})
+
+window.addEventListener("scroll",()=>{
+     var navy=document.getElementById("navy")
+   if(document.documentElement.scrollTop>800)
+      navy.classList.add("sticky-top")
+})
+
 
